f56a5128de2c54f2165e19717770ee3f4b6aba17
Himanshu Makwana <himanshumakwana5634@gmail.com>
2024-09-23 23:55:32 +0530

    frontend navbar setup
    delete app.css,changed in index.css ,make new folder components,and inside components made new folder navbar,in assets add logo png and made one js file for import and export of assets

---
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Navbar.css'
+import { assets } from '../../assets/assets'
+
+function Navbar() {
+  return (
+    <div className='navbar'>
+      <img src={assets.logo} alt="" className="logo" />
+      <ul className="navbar-menu">
+        <li>Home</li>
+        <li>Student Reviews</li>
+        <li>Contect us</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Navbar
